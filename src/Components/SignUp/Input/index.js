55a725d10d45c2f94bd7b48316a8811de4abc98b
unknown <pinoyvj39@gmail.com>
2022-07-16 21:49:36 +0800

    fixed

--- src/Components/SignUp/Input/index.js
+++ src/Components/SignUp/Input/index.js
@@ -1,23 +1,31 @@
-import React,{forwardRef} from "react";
+/* eslint-disable react/display-name */
+import React, { forwardRef } from "react";
 // Components
 import { Input as ChakraInput, Stack, Text } from "@chakra-ui/react";
 
-const Input = forwardRef((props,ref) => {
-  const { label, name, error, ...rest } = props;
-  return (
-    <Stack w={"100%"} mb={"10px !important"}>
-      <Text fontSize={"15px"}>{label}</Text>
-      <ChakraInput
-      borderRadius={"0px"}
-      w={"100%"}
-      borderColor={error[name] && "red"}
-      _focus={{
-        borderColor: error[name] && "red" 
-      }}
-      name={name} ref={ref} {...rest} />
-      {error[name] && <Text color={"red"} fontSize={"14px"}>*{error[name].message}</Text>}
-    </Stack>
-  );
+const Input = forwardRef((props, ref) => {
+	const { label, name, error, ...rest } = props;
+	return (
+		<Stack w={"100%"} mb={"10px !important"}>
+			<Text fontSize={"15px"}>{label}</Text>
+			<ChakraInput
+				borderRadius={"0px"}
+				w={"100%"}
+				borderColor={error[name] && "red"}
+				_focus={{
+					borderColor: error[name] && "red",
+				}}
+				name={name}
+				ref={ref}
+				{...rest}
+			/>
+			{error[name] && (
+				<Text color={"red"} fontSize={"14px"}>
+					*{error[name].message}
+				</Text>
+			)}
+		</Stack>
+	);
 });
 
 export default Input;
--- src/Components/TinyMce/index.js
+++ src/Components/TinyMce/index.js
@@ -1,30 +1,49 @@
-import React, { useRef } from 'react';
+/* eslint-disable react/display-name */
+import React from "react";
 // Tiny Mce
-import { Editor } from '@tinymce/tinymce-react';
+import { Editor } from "@tinymce/tinymce-react";
 
-const TinyMce = React.forwardRef((props,ref) => {
-  return (
-    <>
-    <Editor 
-    onInit={(evt, editor) => ref.current = editor}
-    initialValue={!props?.editValue ? "<p>Start Creating Your Story Now!</p>" : props.editValue}
-    init={{
-      height: 500,
-      menubar: false,
-      plugins: [
-        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
-        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-        'insertdatetime', 'media', 'table', 'preview', 'help', 'wordcount'
-      ],
-      toolbar: 'undo redo | blocks | ' +
-        'bold italic forecolor | alignleft aligncenter ' +
-        'alignright alignjustify | bullist numlist outdent indent | ' +
-        'removeformat | help',
-      content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-    }}
-    />
-    </>
-  )
-})
+const TinyMce = React.forwardRef((props, ref) => {
+	return (
+		<>
+			<Editor
+				onInit={(evt, editor) => (ref.current = editor)}
+				initialValue={
+					!props?.editValue ? "<p>Start Creating Your Story Now!</p>" : props.editValue
+				}
+				init={{
+					height: 500,
+					menubar: false,
+					plugins: [
+						"advlist",
+						"autolink",
+						"lists",
+						"link",
+						"image",
+						"charmap",
+						"anchor",
+						"searchreplace",
+						"visualblocks",
+						"code",
+						"fullscreen",
+						"insertdatetime",
+						"media",
+						"table",
+						"preview",
+						"help",
+						"wordcount",
+					],
+					toolbar:
+						"undo redo | blocks | " +
+						"bold italic forecolor | alignleft aligncenter " +
+						"alignright alignjustify | bullist numlist outdent indent | " +
+						"removeformat | help",
+					content_style:
+						"body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+				}}
+			/>
+		</>
+	);
+});
 
-export default TinyMce
+export default TinyMce;
